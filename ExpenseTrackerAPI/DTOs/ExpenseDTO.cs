8b10311cbaad070927c831856236834e3b66f439
Harsh Kumar <harshkumar@yes-bm.com>
2026-03-21 16:10:02 +0530

    Changes made by harsh

--- ExpenseTrackerAPI/DTOs/ExpenseDTO.cs
+++ ExpenseTrackerAPI/DTOs/ExpenseDTO.cs
@@ -9,5 +9,19 @@ namespace ExpenseTrackerAPI.DTOs
         public string Category { get; set; }
         public DateTime Date { get; set; }
         public string Notes { get; set; }
+
+        /* kjcbgsjdg fdnkl fdklv nfdkljv kj kjdfnkjvfdm vk
+         * vdkfjnvkdfkvnjk dfkjnvk n kvjdfnv bkbvvkjf
+         * vnfkdjnvnkvjldfnv
+         * fgfdbfdhbgfhbgfgngfn fnfg dnfgfg
+         * nbfgggggg 
+         * nfjmgh
+         * k m
+         * g 
+         *  gh
+         *   ujh
+         *   ,jhmghl;knjifgblofgi lfoig hbl gkkfgb
+         *   gfbhlkmgfbn
+         */
     }
 }
